dd2532c411fe21ab629aeccc994adc3c7dfd32e5
Vinay Dargar <vinaydargar@users.noreply.github.com>
2014-07-05 21:02:14 +0530

    Completed Server Queue, Removed Client Queue. Broadcasting and custom stuff implemented. Basic stuff done and tested. yay!

--- qstp/Message.java
+++ qstp/Message.java
@@ -1,14 +1,14 @@
 package qstp;
 
-import java.io.Serializable;
+import java.util.Scanner;
 
 /**
  *
  * @author Vinay
  */
-public class Message implements Serializable {
-    String messageText;
+public class Message {
     String sender;
+    String messageText;
     String destination;
     SimpleClientConnection destinationSCC;
     public Message(String nameOfSender, String messageToSend, String dest) {
@@ -20,9 +20,20 @@ public class Message implements Serializable {
         messageText=messageToSend;
         sender=nameOfSender;
         destinationSCC=destSCC;
+        destination="mID: "+destSCC.mId;
     }
     public Message(String nameOfSender, String messageToSend) {
         messageText=messageToSend;
         sender=nameOfSender;
+    }
+    public Message(String toParse) {
+        Scanner in = new Scanner(toParse);
+        in.useDelimiter("~");
+        sender=in.next();
+        messageText=in.next();
+        destination=in.next();
+    }
+    public String getString() {
+        return (sender+"~"+messageText+"~"+destination);
     }
 }

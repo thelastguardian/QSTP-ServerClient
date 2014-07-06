432b35e18163cea740bf3821bf58995872476c78
Vinay Dargar <vinaydargar@gmail.com>
2014-07-06 15:34:05 +0530

    Code styling and auto-beautification.

--- qstp/Message.java
+++ qstp/Message.java
@@ -7,33 +7,39 @@ import java.util.Scanner;
  * @author Vinay
  */
 public class Message {
+
     String sender;
     String messageText;
     String destination;
     SimpleClientConnection destinationSCC;
+
     public Message(String nameOfSender, String messageToSend, String dest) {
-        messageText=messageToSend;
-        sender=nameOfSender;
-        destination=dest;
+        messageText = messageToSend;
+        sender = nameOfSender;
+        destination = dest;
     }
+
     public Message(String nameOfSender, String messageToSend, SimpleClientConnection destSCC) {
-        messageText=messageToSend;
-        sender=nameOfSender;
-        destinationSCC=destSCC;
-        destination="mID: "+destSCC.mId;
+        messageText = messageToSend;
+        sender = nameOfSender;
+        destinationSCC = destSCC;
+        destination = "mID: " + destSCC.mId;
     }
+
     public Message(String nameOfSender, String messageToSend) {
-        messageText=messageToSend;
-        sender=nameOfSender;
+        messageText = messageToSend;
+        sender = nameOfSender;
     }
+
     public Message(String toParse) {
         Scanner in = new Scanner(toParse);
         in.useDelimiter("~");
-        sender=in.next();
-        messageText=in.next();
-        destination=in.next();
+        sender = in.next();
+        messageText = in.next();
+        destination = in.next();
     }
+
     public String getString() {
-        return (sender+"~"+messageText+"~"+destination);
+        return (sender + "~" + messageText + "~" + destination);
     }
 }
